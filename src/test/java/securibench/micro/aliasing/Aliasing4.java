12759a61c7dfe22f3a8804f81e7f4bc13f785831
jose clavo tafur <jclavotafur@gmail.com>
2024-09-29 19:41:18 -0300

    update number of sinks in Aliasing4

--- src/test/java/securibench/micro/aliasing/Aliasing4.java
+++ src/test/java/securibench/micro/aliasing/Aliasing4.java
@@ -16,7 +16,7 @@ import securibench.micro.MicroTestCase;
 
 /** 
  *  @servlet description="simple aliasing with casts" 
- *  @servlet vuln_count = "1" 
+ *  @servlet vuln_count = "2" 
  *  */
 public class Aliasing4 extends BasicTestCase implements MicroTestCase {
 	private static final String FIELD_NAME = "name";
@@ -38,6 +38,6 @@ public class Aliasing4 extends BasicTestCase implements MicroTestCase {
     }
     
     public int getVulnerabilityCount() {
-        return 1;
+        return 2;
     }
 }
